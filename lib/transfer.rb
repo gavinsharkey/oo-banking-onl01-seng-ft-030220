978af7cee3994c2534e39ab1dccae2b06b6a8ed6
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-12 20:40:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if self.status == 'completed'
+    if self.status == 'complete'
       self.sender.deposit(self.amount)
       self.receiver.balance -= self.amount
       self.status = 'reversed'
